8bb3fd36b450047100ac59908a9fdda53995aecb
jored93 <eduardorrala@live.com>
2022-02-25 17:53:56 -0500

    Todo List with React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
 import Navigation from './components/Navigation'; 
+import React, { Component } from 'react';
 
-function App() {
+import * as  todos  from './todos.json';
+
+class App extends Component {
+  constructor(){
+      super();
+      this.state = {
+        todos
+      }
+    }
+  render() {
+  this.state.todos.map((todo, i) => {
+    return (
+
+    )
+  })
   return (
     <div className="App">
       <Navigation titulo = "Mi primera navegación"/>
-
-      
     </div>
   );
+  }
 }
 
 export default App;
